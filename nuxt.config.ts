f7a776c13a906b09c137049d646b490d4e5930b7
Lewis Hopkins <lewis.hopkins@byojet.com>
2024-04-26 17:03:07 +1000

    set up for ssg

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ import tailwindTypography from '@tailwindcss/typography';
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   srcDir: "src/",
+  ssr: false,
   devtools: { enabled: true },
   modules: ["@nuxt/ui"],
   ui: {
