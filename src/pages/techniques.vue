82054d13eef8ea84c74e9eb17434de61d5333424
lulzhipsters <lewis.j.hopkins@gmail.com>
2024-11-01 22:35:39 +1000

    Add content list to techniques page

--- src/pages/techniques.vue
+++ src/pages/techniques.vue
@@ -1,1 +1,8 @@
-<template></template>
+<template>
+  <ContentList path="/techniques" v-slot="{ list }">
+    <div v-for="technique in list" :key="technique._path">
+      <h2>{{ technique.title }}</h2>
+      <a :href="technique._path">more...</a>
+    </div>
+  </ContentList>
+</template>
