439489874b2de431c118dc1dbce941edc10b8f55
lulzhipsters <lewis.j.hopkins@gmail.com>
2025-04-13 07:08:07 +0000

    Remove the nuxt stuff before we try out astro

--- README.md
+++
@@ -1,7 +0,0 @@
-# Gastronomy Web
-Site version of [Gastronomy](https://github.com/lulzhipsters/gastronomy)
-
-# TODOs
-[] Actions to build based on:
-    - Changes in this repo
-    - Changes in base repo
--- src/pages/techniques.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <ContentList path="/techniques" v-slot="{ list }">
-    <div v-for="technique in list" :key="technique._path">
-      <h2>{{ technique.title }}</h2>
-      <a :href="technique._path">more...</a>
-    </div>
-  </ContentList>
-</template>
